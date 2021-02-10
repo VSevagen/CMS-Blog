01567480cfb032160120781475e19d937c9ce9fd
sevagenv <sevagenv@gmail.com>
2021-02-10 14:10:36 +0400

    Update BlogFormat

--- client/src/components/BlogFormat.jsx
+++ client/src/components/BlogFormat.jsx
@@ -4,12 +4,22 @@ import Footer from './Footer'
 import "../styles/blog.css"
 import GnomeAsia from './GnomeAsia'
 import SchoolVisit from './SchoolVisit'
+import Incubate from './Incubate'
+import MySQLErr from './MySQLErr'
+import MediaWiki from './MediaWiki'
+import Python from './Python'
+import FossTalk from './FossTalk'
 
 function BlogFormat(props) {
 
     const components = {
         GnomeAsia: GnomeAsia,
         SchoolVisit: SchoolVisit,
+        IncubateIND: Incubate,
+        MySQL: MySQLErr,
+        MediaWiki: MediaWiki,
+        Python: Python,
+        FossTalk: FossTalk
         }
     const Blog = React.createElement(components[props.location.aboutProps.component])
     
