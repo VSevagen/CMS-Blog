206fbe8972ecf97957f68f950cf8e3aa2a15317b
sevagenv <sevagenv@gmail.com>
2021-02-20 20:22:17 +0400

    Style out login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-
+import "../styles/login.css"
 
 
 function Login() {
@@ -34,13 +34,25 @@ function Login() {
     }
 
     return(
-        <div>
-            <label>Enter username:</label>
-            <input type="text" value={user} onChange={handleUser}></input>
-            <label>Enter password:</label>
-            <input type="password" value={password} onChange={handlePass}></input>
-            <button type="button" onClick={handleSubmit}>Submit</button>
-        </div>
+        // <div className="">
+        //     <label>Enter username:</label>
+        //     <input type="text" value={user} onChange={handleUser}></input>
+        //     <label>Enter password:</label>
+        //     <input type="password" value={password} onChange={handlePass}></input>
+        //     <button type="button" onClick={handleSubmit}>Submit</button>
+        // </div>
+
+<div class="container">
+<div class="top"></div>
+<div class="bottom"></div>
+<div class="center">
+  <h2>Login</h2>
+  <input type="text" value={user} onChange={handleUser} placeholder="Username"></input>
+  <input type="password" value={password} onChange={handlePass} placeholder="Password"></input>
+  <button className="submit" type="button" onClick={handleSubmit}>Submit</button>
+  <h2>&nbsp;</h2>
+</div>
+</div>
     );
 }
 
