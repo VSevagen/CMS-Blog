deb81c31b4caf0309772e3727b725a91e167ed80
sevagenv <sevagenv@gmail.com>
2021-02-22 15:26:36 +0400

    Query password from collection

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { gql, useQuery } from '@apollo/client';
+import Loader from "react-loader-spinner"
 import "../styles/login.css"
 
+const LOGIN_INFO = gql`
+    query login {
+        login {
+            username
+            password
+        }
+    }
+`;
 
 function Login() {
 
     const[user, Setuser] = useState('');
     const[password, Setpass] = useState('');
     const history = useHistory();
+
+    const {loading, error, data} = useQuery(LOGIN_INFO);
+    if(loading) return <div className="spinner"><Loader type="Grid" color="#9c9c9c" height={80} width={80}/></div>
+    if(error) return `Error! ${error.message}`;
+
+    console.log(data.login[0].username);
+
     function handleSubmit() {
 
-        if(user === "sevagen" && password === "sevagen") {
+        if(user === data.login[0].username && password === data.login[0].password) {
             history.push({
                 pathname: '/admin',
                 state: {authenticated: true}
