40777a6f29f8312cb0fc77bc12cb98f0bff2c61d
sevagenv <sevagenv@gmail.com>
2021-02-24 16:45:47 +0400

    Error msg when using wrong credential

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -17,26 +17,22 @@ function Login() {
 
     const[user, Setuser] = useState('');
     const[password, Setpass] = useState('');
+    const[isCred, setCred] = useState(false)
     const history = useHistory();
 
     const {loading, error, data} = useQuery(LOGIN_INFO);
     if(loading) return <div className="spinner"><Loader type="Grid" color="#9c9c9c" height={80} width={80}/></div>
     if(error) return `Error! ${error.message}`;
 
-    console.log(data.login[0].username);
-
-    function handleSubmit() {
-
+    function handleSubmit(e) {
         if(user === data.login[0].username && password === data.login[0].password) {
             history.push({
                 pathname: '/admin',
                 state: {authenticated: true}
             });
         } else {
-            history.push({
-                pathname: "/",
-                state: {login: false}
-            })
+            e.preventDefault()
+            setCred(true);
         }
         Setuser('');
         Setpass('');
@@ -67,6 +63,7 @@ function Login() {
   <input type="text" value={user} onChange={handleUser} placeholder="Username"></input>
   <input type="password" value={password} onChange={handlePass} placeholder="Password"></input>
   <button className="submit" type="button" onClick={handleSubmit}>Submit</button>
+  {isCred ? <p className="errorMsg">You have entered the wrong credentials</p> : null}
   <h2>&nbsp;</h2>
 </div>
 </div>
