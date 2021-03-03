fdc1188f210867f7c1c45f7eafcab5c0dca911e7
sevagenv <sevagenv@gmail.com>
2021-03-03 21:12:15 +0400

    Remove unecessary lines

--- client/src/components/BlogFormat.jsx
+++ client/src/components/BlogFormat.jsx
@@ -2,21 +2,8 @@ import React from 'react'
 import Header from './Header';
 import Footer from './Footer'
 import "../styles/blog.css"
-import Incubate from './Incubate'
-import MySQLErr from './MySQLErr'
-import MediaWiki from './MediaWiki'
-import Python from './Python'
-import FossTalk from './FossTalk'
 
 function BlogFormat(props) {
-
-    const components = {
-        IncubateIND: Incubate,
-        MySQL: MySQLErr,
-        MediaWiki: MediaWiki,
-        Python: Python,
-        FossTalk: FossTalk
-        };
 
     if (props.location.aboutProps != undefined) {
         let blogName = props.location.aboutProps.component;
@@ -33,14 +20,13 @@ function BlogFormat(props) {
         return {__html: localStorage.getItem('text')}
     }
 
-    const Blog = React.createElement(components[localStorage.getItem('blogName')]);
         return(
             <div>
                   <Header />
                     <div className="post-list">
                         <ul>
                             <li>
-        <div className="blog-title">{localStorage.getItem('title')}</div>
+                                <div className="blog-title">{localStorage.getItem('title')}</div>
                                 <div className="blog-date">{localStorage.getItem('date')}</div>
 
                                 <div>
