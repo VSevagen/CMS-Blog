8c5cd5e30e335c96b4ad94f942935bd43d66ce42
sevagenv <sevagenv@gmail.com>
2021-03-03 21:13:10 +0400

    Revamp blog creating form and added a live markup preview

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -17,6 +17,7 @@ mutation createBlog($title: String!, $description: String!, $text: String!, $com
 
 function Admin() {
 
+    let marked = require("marked");
     const location = useLocation();
     let authenticated = null;
     if(location.state === undefined) {
@@ -25,6 +26,10 @@ function Admin() {
         authenticated = true;
     }
 
+    var center = {
+        margin: "0 auto",
+        width: "70%"
+    }
     const [addBlog] = useMutation(CREATE_NEW_BLOG)
     const[title, setTitle] = useState('')
     const[description, setDesc] = useState('')
@@ -64,49 +69,94 @@ function Admin() {
     return(
 
     <div>
-    {authenticated ? <div>
-        <h2>Create a new blog</h2>
+    {authenticated ? 
+    <div>
+        <div><h2>Create a new blog</h2></div>
 
-    <div id="container">
+        <div style={center}>
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Blog Title</span>
+            <input type="text" class="form-control" placeholder="Ex: An Overview of India's education system" value={title} onChange={handleChange}></input>
+        </div>
 
-    <span className="input">
-        <input type="text" className="input__field" id="input-1" value={title} onChange={handleChange} />
-        <label for="input-1" className="input__label">
-        <span className="input__label-content">Blog Title</span>
-    </label>
-    </span>
+        <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon2">Component Name</span>
+            <input type="text" class="form-control" placeholder="Recipient's username" value={component} onChange={handleChange4}></input>
+        </div>
 
-    <span className="input">
-        <input type="text" className="input__field" id="input-2" value={component} onChange={handleChange4} />
-        <label for="input-2" className="input__label">
-        <span className="input__label-content">Component Name</span>
-        </label>
-    </span>
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon3">Date</span>
+            <input type="text" class="form-control" value={date} onChange={handleChange5} placeholder="Ex: 22 Jun, 2019"></input>
+        </div>
 
-    <span className="input">
-        <input type="text" className="input__field" id="input-3" value={date} onChange={handleChange5}/>
-        <label for="input-3" className="input__label">
-        <span className="input__label-content">Date</span>
-        </label>
-    </span>
+        <div class="input-group mb-3">
+            <span class="input-group-text">Description</span>
+            <input type="text" class="form-control" placeholder="Ex: Let me start by saying that the current statistics are showing that the education system is outdated, inefficient and a danger to human health" value={description} onChange={handleChange2}></input>
+        </div>
 
-    <span className="input">
-        <input type="text" className="input__field" id="input-4" value={description} onChange={handleChange2}/>
-        <label for="input-4" className="input__label">
-        <span className="input__label-content">Description</span>
-        </label>
-    </span>
+        <div class="row">
 
-    <span className="input message">
-        <input component="textarea" className="input__field" id="input-5" value={text} onChange={handleChange3}></input>
-        <label for="input-5" className="input__label">
-        <span className="input__label-content">Text</span>
-        </label>
-    </span>
+            <div class="input-group col-lg-6">
+                <span class="input-group-text">Blog Text</span>
+                <textarea rows="20" placeholder="IMPORTANT......use the following class for you text. blog_img for images and blog-content"  class="form-control" aria-label="With textarea" value={text} onChange={handleChange3}></textarea>
+            </div>
 
-    <button id="send-button" type="button" onClick={handleSubmit}>Submit</button>
-    </div>   
-    </div> :
+            <div class="col-lg-6 preview" dangerouslySetInnerHTML={{__html: marked(text)}}></div>
+
+        </div>
+
+        
+        <button type="button" id="center" class="btn btn-outline-secondary" onClick={handleSubmit}>Submit</button>
+
+
+        </div>
+    </div>
+    
+    // <div>
+    //     <h2>Create a new blog</h2>
+
+    // <div id="container">
+
+    // <span className="input">
+    //     <input type="text" className="input__field" id="input-1" value={title} onChange={handleChange} />
+    //     <label for="input-1" className="input__label">
+    //     <span className="input__label-content">Blog Title</span>
+    // </label>
+    // </span>
+
+    // <span className="input">
+    //     <input type="text" className="input__field" id="input-2" value={component} onChange={handleChange4} />
+    //     <label for="input-2" className="input__label">
+    //     <span className="input__label-content">Component Name</span>
+    //     </label>
+    // </span>
+
+    // <span className="input">
+    //     <input type="text" className="input__field" id="input-3" value={date} onChange={handleChange5}/>
+    //     <label for="input-3" className="input__label">
+    //     <span className="input__label-content">Date</span>
+    //     </label>
+    // </span>
+
+    // <span className="input">
+    //     <input type="text" className="input__field" id="input-4" value={description} onChange={handleChange2}/>
+    //     <label for="input-4" className="input__label">
+    //     <span className="input__label-content">Description</span>
+    //     </label>
+    // </span>
+
+    // <span className="input">
+    //     <div>
+    //         <textarea style={inputStyle} value={text} onChange={handleChange3}>
+    //         </textarea>
+    //     </div>
+    //     <div style={outputStyle} dangerouslySetInnerHTML={{__html: marked(text)}}></div>
+    // </span>
+
+    // <button id="send-button" type="button" onClick={handleSubmit}>Submit</button>
+    // </div>   
+    // </div> 
+    :
     <div class="bsod container">
     <h1 class="neg title"><span class="bg">Unauthorised</span></h1>
     <p className="entry">You tried to access /admin without login in.</p>
