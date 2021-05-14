5300208ba9430cc8b96be28737f4ddd41530cd50
sevagenv <sevagenv@gmail.com>
2021-05-14 20:11:51 +0400

    Add authorization token to access graphql server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
-import { InMemoryCache } from "apollo-boost";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import Routes from "./components/Routes";
 
+const { REACT_APP_JWT_ACCESS_TOKEN } = process.env;
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_SERVER_URL || "http://localhost:5000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: REACT_APP_JWT_ACCESS_TOKEN,
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_SERVER_URL,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   onError: ({ networkError, graphQLErrors }) => {
     console.log("graphQLErrors", graphQLErrors);
